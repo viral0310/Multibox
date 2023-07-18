1b6cee639fad4bb6366773ab9fa38e8c38e62ce9
viral0310 <viraltada2001@gmail.com>
2023-07-18 18:57:51 +0530

    main

--- lib/Screen/HomeScreens/HomeScreens.dart
+++ lib/Screen/HomeScreens/HomeScreens.dart
@@ -21,9 +21,17 @@ class _HomeScreenState extends State<HomeScreen> {
           children: [
             Container(
               width: width,
-              height: height / 4,
-              decoration: const BoxDecoration(
-                gradient: LinearGradient(
+              height: height / 6,
+              decoration: BoxDecoration(
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey.withOpacity(0.5),
+                    spreadRadius: 2,
+                    blurRadius: 5,
+                    offset: const Offset(0, 3),
+                  ),
+                ],
+                gradient: const LinearGradient(
                   begin: Alignment(-1, -1),
                   end: Alignment(1, 1),
                   colors: <Color>[
@@ -35,10 +43,56 @@ class _HomeScreenState extends State<HomeScreen> {
                   stops: <double>[0, 0, 0.484, 1],
                 ),
               ),
-              child: const Column(
-                children: [],
+              child: Padding(
+                padding: const EdgeInsets.only(left: 20, top: 25),
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Image.asset(
+                      "assets/images/shopping-logo 1.png",
+                      height: 60,
+                    ),
+                    const Text(
+                      ' MultiBox POS',
+                      style: TextStyle(
+                        fontSize: 22,
+                        fontWeight: FontWeight.w600,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ),
+            Padding(
+              padding: const EdgeInsets.only(left: 16, right: 16),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  DrawerIcons("assets/images/Drawer/checkout.png", 'Checkout'),
+                  DrawerIcons(
+                      "assets/images/Drawer/analytics.png", 'Dashboard'),
+                  DrawerIcons(
+                      "assets/images/Drawer/contact-book.png", 'Contact'),
+                  DrawerIcons(
+                      "assets/images/Drawer/business-report.png", 'Report'),
+                  DrawerIcons("assets/images/Drawer/insurance-company.png",
+                      'Companies'),
+                  DrawerIcons("assets/images/Drawer/settings.png", 'Setting'),
+                  DrawerIcons("assets/images/Drawer/group.png", 'Team'),
+                  Divider(),
+                  Text(
+                    'Comumunicate',
+                    textAlign: TextAlign.start,
+                  ),
+                  DrawerIcons(
+                      "assets/images/Drawer/share.png", 'Share This App'),
+                  DrawerIcons(
+                      "assets/images/Drawer/send-mail.png", 'Contact Support'),
+                  DrawerIcons("assets/images/Drawer/rating.png", 'Rate Us'),
+                ],
+              ),
+            )
           ],
         ),
       ),
@@ -47,4 +101,40 @@ class _HomeScreenState extends State<HomeScreen> {
       ),
     );
   }
+}
+
+Widget DrawerIcons(String image, String text) {
+  return Builder(builder: (context) {
+    return SizedBox(
+      height: 45,
+      child: Padding(
+        padding: const EdgeInsets.only(left: 10),
+        child: Row(
+          children: [
+            SizedBox(
+              width: 30,
+              child: Image.asset(
+                image,
+                color: Colors.black87,
+                height: 28,
+                width: 28,
+              ),
+            ),
+            const SizedBox(
+              width: 15,
+            ),
+            SizedBox(
+              child: Text(
+                text,
+                style: const TextStyle(
+                  fontSize: 16,
+                  color: Colors.black87,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  });
 }
